1522e96708c9e9e152dab4a071e2c8a6c80ca569
José Darío González Cruz <darioucv@ucvvirtual.edu.pe>
2024-01-08 15:33:31 -0500

    init

---
+++ src/main/java/com/liceotrujillo/apiclt/ApicltApplication.java
@@ -0,0 +1,13 @@
+package com.liceotrujillo.apiclt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicltApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liceotrujillo/apiclt/ApicltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liceotrujillo.apiclt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
